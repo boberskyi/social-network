d1e279e079258220b7085b4a1b0dd3affe92023a
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-07-15 12:42:36 +0200

    change structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import {NavigationLeft} from "./components/NavigationLeft";
+import {MainBlock} from "./components/MainBlock";
 
 function App() {
-  return (
-    <div className="App">
-      <NavigationLeft/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavigationLeft/>
+            <MainBlock/>
+        </div>
+    );
 }
 
 export default App;
