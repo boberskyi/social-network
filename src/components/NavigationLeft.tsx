9b0141b16652c6fa1ebe22956c914b78409c14aa
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-07-15 11:44:09 +0200

    menu items

--- src/components/NavigationLeft.tsx
+++ src/components/NavigationLeft.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import styled from "styled-components";
 import {TbHexagonLetterS} from "react-icons/tb";
+import {ImNewspaper} from "react-icons/im";
+import {MdGroups, MdOutlineForum, MdOutlineViewTimeline} from "react-icons/md";
+import {FaUserFriends} from "react-icons/fa";
+import {TfiGallery} from "react-icons/tfi";
+import {LiaVideoSolid} from "react-icons/lia";
 
 export const NavigationLeft = () => {
     return (
@@ -8,6 +13,37 @@ export const NavigationLeft = () => {
             <StyledNavLeftTop>
                 <TbHexagonLetterS />
             </StyledNavLeftTop>
+
+            <StyledNavLeftBtm>
+                <StyledNavLeftLink>
+                    <ImNewspaper/>
+                    <StyledNavLeftLinkTitle>Newsfeed</StyledNavLeftLinkTitle>
+                </StyledNavLeftLink>
+                <StyledNavLeftLink>
+                    <MdOutlineViewTimeline/>
+                    <StyledNavLeftLinkTitle>Member timeline</StyledNavLeftLinkTitle>
+                </StyledNavLeftLink>
+                <StyledNavLeftLink>
+                    <MdGroups/>
+                    <StyledNavLeftLinkTitle>Groups</StyledNavLeftLinkTitle>
+                </StyledNavLeftLink>
+                <StyledNavLeftLink>
+                    <FaUserFriends/>
+                    <StyledNavLeftLinkTitle>Friends</StyledNavLeftLinkTitle>
+                </StyledNavLeftLink>
+                <StyledNavLeftLink>
+                    <TfiGallery/>
+                    <StyledNavLeftLinkTitle>Gallery</StyledNavLeftLinkTitle>
+                </StyledNavLeftLink>
+                <StyledNavLeftLink>
+                    <LiaVideoSolid/>
+                    <StyledNavLeftLinkTitle>Videos</StyledNavLeftLinkTitle>
+                </StyledNavLeftLink>
+                <StyledNavLeftLink>
+                    <MdOutlineForum/>
+                    <StyledNavLeftLinkTitle>Forums</StyledNavLeftLinkTitle>
+                </StyledNavLeftLink>
+            </StyledNavLeftBtm>
         </StyledNavLeft>
     );
 };
@@ -34,4 +70,24 @@ const StyledNavLeftTop = styled.a`
   transition: all .3s ease-in-out;
   color: #fff;
   font-size: 50px;
+`
+const StyledNavLeftBtm = styled.nav`
+  padding: 30px 0;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  gap: 20px;
+`
+const StyledNavLeftLink = styled.a`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  color: #8793a3;
+  padding: 15px 0;
+  border-bottom: 1px solid rgba(135, 147, 143, .2);
+`
+const StyledNavLeftLinkTitle = styled.span`
+  display: none;
 `
