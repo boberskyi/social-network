1670929436b50d826d3aa9c76c1767457ef2f2b7
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-07-15 12:43:19 +0200

    create and basic styles of main block

---
+++ src/components/MainBlock.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Banner} from "./Banner";
+import styled from "styled-components";
+
+export const MainBlock = () => {
+    return (
+        <StyledMainBlock>
+            <Banner/>
+        </StyledMainBlock>
+    );
+};
+
+const StyledMainBlock = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 20px 50px 0 138px; //right +88px as nav left
+  background-color: #eff4fb;
+  min-height: 100vh;
+`
